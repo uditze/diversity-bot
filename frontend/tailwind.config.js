c99259c628724d5fe4055a29f91e9aa4a3ae03f8
uditze <udi.tsem@gmail.com>
2025-06-04 21:36:21 +0300

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './App.jsx'],
+  content: [
+    './index.html',
+    './*.{js,jsx,ts,tsx}',
+    './**/*.{js,jsx,ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
   darkMode: 'media',
   plugins: [],
-}
+};
