e1401d3e6cc6d0091eb76b155420e06c2659907f
uditze <udi.tsem@gmail.com>
2025-06-16 14:30:26 +0300

    Update supabaseClient.js

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -7,8 +7,7 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Supabase URL or Key is not defined in environment variables.");
+  console.log("Supabase credentials not found for this service.");
 }
 
-// יצירת והגדרת הלקוח של Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
